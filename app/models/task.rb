3ed6f2173ebef71aa256081f8da07e43c9ed2662
brenothales <breno.thales@gmail.com>
2016-07-07 20:24:21 +0000

    finalizando o aplicação

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ActiveRecord::Base
     belongs_to :user
+    validates_presence_of :content, :user_id, message: 'Campo Obrigatorio'
+	
+	validates     :content_html, presence: true
+	validates     :user_id, presence: true
+	
+     auto_html_for :content do
+		html_escape
+		image
+		youtube(:width => "100%", :height => 250, :autoplay => false)
+		link :target => "_blank", :rel => "nofollow"
+		simple_format
+	end
+	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623174004) do
+ActiveRecord::Schema.define(version: 20160707195112) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "titulo"
@@ -25,10 +25,11 @@ ActiveRecord::Schema.define(version: 20160623174004) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.integer  "user_id"
-    t.string   "state",      default: "to_do"
+    t.string   "state",        default: "to_do"
+    t.text     "content_html"
   end
 
   create_table "users", force: :cascade do |t|
